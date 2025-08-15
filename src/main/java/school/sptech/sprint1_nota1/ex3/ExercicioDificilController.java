e687dc083a8177fc986b25277687683d6abacf0b
GuilhermeAugustoFigueiredo <guilherme.figueiredo@sptech.school>
2025-08-15 18:50:33 -0300

    atividade finalizada

--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -9,6 +9,27 @@ public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
     public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
-        return null;
+
+        if (n <= 0){
+            return new ExercicioDificilResponse(0,0);
+        }
+
+        if (n <= 1){
+            return new ExercicioDificilResponse(1,1);
+        }
+
+        Integer nAnterior = 0;
+        Integer nAtual = 1;
+        Integer soma = 1;
+
+        for (int i = 2; i <= n; i++) {
+            Integer proximoN = nAnterior + nAtual;
+            soma += proximoN;
+
+            nAnterior = nAtual;
+            nAtual = proximoN;
+        }
+
+        return new ExercicioDificilResponse(nAtual, soma);
     }
 }
